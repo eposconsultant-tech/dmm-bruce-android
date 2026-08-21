838120fad6af27e20b04125b411c941713da9313
eposconsultant-tech <eposconsultant@gmail.com>
2026-08-22 00:26:55 +0100

    Link DMM Driver app to live dmm.aigoat.uk cloud portal

--- app/src/main/java/uk/aigoat/dmmdriver/MainActivity.java
+++ app/src/main/java/uk/aigoat/dmmdriver/MainActivity.java
@@ -1,9 +1,12 @@
 package uk.aigoat.dmmdriver;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -12,13 +15,16 @@ import android.widget.FrameLayout;
 import android.graphics.Color;
 
 public class MainActivity extends Activity {
-    private static final String DMM_URL = "https://stalwart-taiyaki-379722.netlify.app/";
+    private static final String DMM_URL = "https://dmm.aigoat.uk/";
+    private static final int FILE_CHOOSER_REQUEST = 2001;
     private WebView webView;
+    private ValueCallback<Uri[]> filePathCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         immersive();
+
         webView = new WebView(this);
         webView.setBackgroundColor(Color.WHITE);
         FrameLayout frame = new FrameLayout(this);
@@ -37,16 +43,51 @@ public class MainActivity extends Activity {
         settings.setLoadWithOverviewMode(true);
         settings.setMediaPlaybackRequiresUserGesture(false);
         settings.setJavaScriptCanOpenWindowsAutomatically(true);
-        settings.setUserAgentString(settings.getUserAgentString() + " DMM-Android-Driver/1.0");
+        settings.setUserAgentString(settings.getUserAgentString() + " DMM-Android-Driver/2.0");
 
         CookieManager cookies = CookieManager.getInstance();
         cookies.setAcceptCookie(true);
         cookies.setAcceptThirdPartyCookies(webView, true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
 
-        if (savedInstanceState == null) webView.loadUrl(DMM_URL);
-        else webView.restoreState(savedInstanceState);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                if (MainActivity.this.filePathCallback != null) {
+                    MainActivity.this.filePathCallback.onReceiveValue(null);
+                }
+                MainActivity.this.filePathCallback = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                try {
+                    startActivityForResult(intent, FILE_CHOOSER_REQUEST);
+                    return true;
+                } catch (Exception ex) {
+                    MainActivity.this.filePathCallback = null;
+                    return false;
+                }
+            }
+        });
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(DMM_URL);
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FILE_CHOOSER_REQUEST) {
+            if (filePathCallback != null) {
+                Uri[] results = WebChromeClient.FileChooserParams.parseResult(resultCode, data);
+                filePathCallback.onReceiveValue(results);
+                filePathCallback = null;
+            }
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void immersive() {
@@ -56,16 +97,19 @@ public class MainActivity extends Activity {
             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 
-    @Override public void onBackPressed() {
+    @Override
+    public void onBackPressed() {
         if (webView != null && webView.canGoBack()) webView.goBack(); else super.onBackPressed();
     }
 
-    @Override protected void onSaveInstanceState(Bundle outState) {
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
         if (webView != null) webView.saveState(outState);
         super.onSaveInstanceState(outState);
     }
 
-    @Override public void onWindowFocusChanged(boolean hasFocus) {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) immersive();
     }
